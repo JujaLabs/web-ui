0cbfd7efea39e181ec7edb8db59239828e0cbfbb
MaksKulikov <kulykov.maks@gmail.com>
2017-05-01 23:04:26 +0300

    created method getPointSumForAllUsers() and converts them into an array UserActivity

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,5 @@
 import {Component, HostListener, OnInit} from '@angular/core';
-import { GamificationService}                    from '../../service/gamification.service';
+import { GamificationService}            from '../../service/gamification.service';
 import {UserActivity}                    from '../../model/userActivity';
 
 export enum KEY_CODE {
@@ -29,8 +29,18 @@ export class TableComponent implements OnInit{
 
     private getPointSumForAllUsers() {
         this.gamificationService.getPointSumForAllUsers().subscribe(data => {
-            this.users = data;
+            this.convertData(data);
         });
+    }
+
+    private convertData(data: Array<any>) {
+        data.forEach(element => {
+            element.uid = element.to;
+            element.pointSum = element.point;
+            delete element.to;
+            delete element.point;
+        });
+        this.users = data;
     }
 
     @HostListener('window:keyup', ['$event'])
@@ -49,9 +59,9 @@ export class TableComponent implements OnInit{
         this.selectedIndex = i
     }
 
-    setKey = (th: any) => {
+    setKey = (th: string) => {
         this.counter === 2 ? this.counter = 0 : this.counter++;
-        th === 'uid' ? this.key = 'uid' : this.key = 'point';
+        th === 'uid' ? this.key = 'uid' : this.key = 'pointSum';
     };
 }
 
