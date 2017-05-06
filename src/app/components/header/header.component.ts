84b1a7d6f6ee0566fa18c435bdb6eae31021509e
starikivan <peter.v.kramar@gmail.com>
2017-05-06 18:03:42 +0300

    #13: Create component all-users-table. Add header.

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: 'app/components/header/header.component.html'
+})
+
+export class HeaderComponent {
+}
