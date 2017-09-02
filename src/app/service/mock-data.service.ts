9d7a24d5dde307487d3cf986c0d3efcfeef169a7
MaksKulikov <kulykov.maks@gmail.com>
2017-09-02 17:49:12 +0300

    created teams template

--- src/app/service/mock-data.service.ts
+++ src/app/service/mock-data.service.ts
@@ -49,6 +49,46 @@ export class MockDataService implements InMemoryDbService {
               '02de9640-5900-4805-b384-7cc33ab74081'],
             activateDate: 1502928000000,
             deactivateDate: 1505520000000},
+          { id: '6',
+            members: [
+              '002fafbe-0a96-4c9a-a876-d35a783325c6',
+              '01f1d246-cc99-4658-b256-7cc6631fdde8',
+              '02629c8b-f268-4296-ae6f-ee01a4b0dede',
+              '02de9640-5900-4805-b384-7cc33ab74081'],
+            activateDate: 1502928000000,
+            deactivateDate: 1505520000000},
+          { id: '7',
+            members: [
+              '002fafbe-0a96-4c9a-a876-d35a783325c6',
+              '01f1d246-cc99-4658-b256-7cc6631fdde8',
+              '02629c8b-f268-4296-ae6f-ee01a4b0dede',
+              '02de9640-5900-4805-b384-7cc33ab74081'],
+            activateDate: 1502928000000,
+            deactivateDate: 1505520000000},
+          { id: '8',
+            members: [
+              '002fafbe-0a96-4c9a-a876-d35a783325c6',
+              '01f1d246-cc99-4658-b256-7cc6631fdde8',
+              '02629c8b-f268-4296-ae6f-ee01a4b0dede',
+              '02de9640-5900-4805-b384-7cc33ab74081'],
+            activateDate: 1502928000000,
+            deactivateDate: 1505520000000},
+          { id: '9',
+            members: [
+              '002fafbe-0a96-4c9a-a876-d35a783325c6',
+              '01f1d246-cc99-4658-b256-7cc6631fdde8',
+              '02629c8b-f268-4296-ae6f-ee01a4b0dede',
+              '02de9640-5900-4805-b384-7cc33ab74081'],
+            activateDate: 1502928000000,
+            deactivateDate: 1505520000000},
+          { id: '10',
+            members: [
+              '002fafbe-0a96-4c9a-a876-d35a783325c6',
+              '01f1d246-cc99-4658-b256-7cc6631fdde8',
+              '02629c8b-f268-4296-ae6f-ee01a4b0dede',
+              '02de9640-5900-4805-b384-7cc33ab74081'],
+            activateDate: 1502928000000,
+            deactivateDate: 1505520000000},
         ];
         return {activeKeepers, teams};
     }
